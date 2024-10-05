7038c095ab6e7067d9b0c83cd59b5155f2c63f02
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-05 10:54:30 +0530

    first commit

---
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/NoteCollectorV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.note_collector_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCollectorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCollectorV2Application.class, args);
+	}
+
+}
---
+++ note-collector-V2/src/test/java/lk/ijse/note_collector_V2/NoteCollectorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.note_collector_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteCollectorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
