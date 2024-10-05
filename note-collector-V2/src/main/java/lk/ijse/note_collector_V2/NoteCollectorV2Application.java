c58ad9de1ae6201363da41b143536590d7712f0f
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-05 14:00:16 +0530

    database connectivity

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/NoteCollectorV2Application.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/NoteCollectorV2Application.java
@@ -1,13 +1,19 @@
 package lk.ijse.note_collector_V2;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NoteCollectorV2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteCollectorV2Application.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
