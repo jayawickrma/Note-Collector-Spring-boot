22a5947d005b1acf5799b9418ea47863f0b868ad
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 13:37:07 +0530

    implemented UserDetails pakage to UserEntity file and ovveride it methods

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Entity/EntityIMPL/UserEntity.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Entity/EntityIMPL/UserEntity.java
@@ -2,11 +2,15 @@ package lk.ijse.note_collector_V2.Entity.EntityIMPL;
 
 
 import jakarta.persistence.*;
+import lk.ijse.note_collector_V2.Entity.Role;
 import lk.ijse.note_collector_V2.Entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
@@ -14,7 +18,7 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "user")
-public class UserEntity implements SuperEntity {
+public class UserEntity implements SuperEntity, UserDetails {
     @Id
     public String userID;
     public String firstName;
@@ -26,4 +30,36 @@ public class UserEntity implements SuperEntity {
     public String profilePic;
     @OneToMany(mappedBy = "user")
     public List<NoteEntity>note;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
