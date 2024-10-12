24a9261af1d2ade70102cd4beac179508a5d78b3
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 13:52:40 +0530

    created JWTConfigFuilter file

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/JWTConfigFuilter.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/JWTConfigFuilter.java
@@ -1,4 +1,16 @@
 package lk.ijse.note_collector_V2.Config;
 
-public class JWTConfigFuilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTConfigFuilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
