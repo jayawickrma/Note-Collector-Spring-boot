40c961b333bf6b38c9c444c05c45226467344b3d
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 09:57:29 +0530

    SecurityCoonfig class created

---
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/SecurityCoonfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.note_collector_V2.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityCoonfig {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+    }
+}
