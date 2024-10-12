a634e5ccc6e8a5ab238d94c2afea6b1073538975
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 13:31:03 +0530

    SecureConfig file created and add thm to author request path

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/SecurityCoonfig.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/SecurityCoonfig.java
@@ -1,37 +1,51 @@
 package lk.ijse.note_collector_V2.Config;
 
+import lk.ijse.note_collector_V2.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityCoonfig {
+    private final UserService userService;
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(req ->req.requestMatchers("api/v1/auth/").
+                        permitAll().anyRequest().authenticated())
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider()
+                .addFilterBefore();
+
+        return http.build();
+    }
     @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-            httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                    .authorizeHttpRequests(req->req.requestMatchers("api/v1/auth/**")
-                    .permitAll()
-                            .anyRequest()
-                            .authenticated())
-
-                    .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authenticationProvider()
-                    .addFilterBefore();
-            return httpSecurity.build()
-        }
-        public AuthenticationProvider authenticationProvider(){
-            DaoAuthenticationProvider dap =new DaoAuthenticationProvider();
-
-            dap.setUserDetailsService();
-        }
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userService.userdetailservice());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/service/IMPL/UserServiceIMPL.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/service/IMPL/UserServiceIMPL.java
@@ -6,9 +6,11 @@ import lk.ijse.note_collector_V2.DAO.UserDAO;
 import lk.ijse.note_collector_V2.DTO.IMPL.UserDTO;
 import lk.ijse.note_collector_V2.Entity.EntityIMPL.UserEntity;
 import lk.ijse.note_collector_V2.Exeptions.DataPersistExeption;
+import lk.ijse.note_collector_V2.Exeptions.UserNotFoutnExeptions;
 import lk.ijse.note_collector_V2.Utill.Mapping;
 import lk.ijse.note_collector_V2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -19,61 +21,67 @@ import java.util.Optional;
 @Service
 @Transactional
 public class UserServiceIMPL implements UserService {
-  @Autowired
-   private UserDAO userDAO;
-  @Autowired
-  private Mapping mapping;
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public UserDTO saveUser(UserDTO userDTO) {
-        UserEntity saveuser=userDAO.save(mapping.toUserEntity(userDTO));
-        if (saveuser==null){
-          throw new DataPersistExeption("something went wrong0");
+        UserEntity saveuser = userDAO.save(mapping.toUserEntity(userDTO));
+        if (saveuser == null) {
+            throw new DataPersistExeption("something went wrong0");
         }
         return mapping.touserDTO(saveuser);
 
     }
 
-  @Override
-  public List<UserDTO> getAllUsers() {
-    List<UserEntity> allUsers = userDAO.findAll();
-    return mapping.allUsers(allUsers);
-  }
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<UserEntity> allUsers = userDAO.findAll();
+        return mapping.allUsers(allUsers);
+    }
 
-  @Override
-  public UserDTO getUser(String userId) {
-      if (userDAO.existsById(userId)){
-        UserEntity selectedUser = userDAO.getReferenceById(userId);
-        return mapping.touserDTO(selectedUser);
-      }else {
-        return new SelectedUserStatus(2,"User With ID " + userId + " Not found");
-      }
+    @Override
+    public UserDTO getUser(String userId) {
+        if (userDAO.existsById(userId)) {
+            UserEntity selectedUser = userDAO.getReferenceById(userId);
+            return mapping.touserDTO(selectedUser);
+        } else {
+            return new SelectedUserStatus(2, "User With ID " + userId + " Not found");
+        }
 
-  }
+    }
 
-  @Override
-  public void deleteUser(String userId) {
-      userDAO.deleteById(userId);
+    @Override
+    public void deleteUser(String userId) {
+        userDAO.deleteById(userId);
 
-  }
+    }
 
-  @Override
-  public void updateUser(String userId, UserDTO userDTO) {
-    Optional<UserEntity>id = userDAO.findById(userId);
-      if (id.isPresent()){
-        id.get().setFirstName(userDTO.getFirstName());
-        id.get().setLastName(userDTO.getLastName());
-        id.get().setEmail(userDTO.getEmail());
-        id.get().setPassword(userDTO.getPassword());
-        id.get().setProfilePic(userDTO.getProfilePic());
-      }
-  }
+    @Override
+    public void updateUser(String userId, UserDTO userDTO) {
+        Optional<UserEntity> id = userDAO.findById(userId);
+        if (id.isPresent()) {
+            id.get().setFirstName(userDTO.getFirstName());
+            id.get().setLastName(userDTO.getLastName());
+            id.get().setEmail(userDTO.getEmail());
+            id.get().setPassword(userDTO.getPassword());
+            id.get().setProfilePic(userDTO.getProfilePic());
+        }
+    }
 
     @Override
     public UserDetailsService userdetailservice() {
-        return String username -> {
-             userDAO.findByEmail(username)
-                     .orElseThrow(()-> UsernameNotFoundException)
-        }
+        return username -> {
+            try {
+                return (UserDetails) userDAO.findByEmail(username).orElseThrow(()->new UserNotFoutnExeptions());
+            } catch (UserNotFoutnExeptions e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
+
+
 
