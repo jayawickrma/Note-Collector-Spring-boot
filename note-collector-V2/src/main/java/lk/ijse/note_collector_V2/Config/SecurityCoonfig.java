ed6efa34182a399301c4f18c5a83611d65b16df2
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 10:10:32 +0530

    added username and pw with his role

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/SecurityCoonfig.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/Config/SecurityCoonfig.java
@@ -1,8 +1,12 @@
 package lk.ijse.note_collector_V2.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +19,12 @@ public class SecurityCoonfig {
                 .httpBasic();
         return http.build();
     }
-
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("nishan")
+                .password("nishan2004")
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
