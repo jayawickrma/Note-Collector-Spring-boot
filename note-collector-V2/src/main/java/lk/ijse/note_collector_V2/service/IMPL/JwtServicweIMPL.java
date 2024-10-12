3a72c065f0d1979d8072109f445934d01b2621ac
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-10-12 15:54:44 +0530

    JWTservice interface created and imolementation and implements it to jwtServiceIMPL classs

--- note-collector-V2/src/main/java/lk/ijse/note_collector_V2/service/IMPL/JwtServicweIMPL.java
+++ note-collector-V2/src/main/java/lk/ijse/note_collector_V2/service/IMPL/JwtServicweIMPL.java
@@ -1,9 +1,21 @@
 package lk.ijse.note_collector_V2.service.IMPL;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.websocket.Decoder;
 import lk.ijse.note_collector_V2.service.JWTservice;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
+
+import static org.springframework.security.config.Elements.JWT;
+
+@Service
 public class JwtServicweIMPL implements JWTservice {
+
     @Override
     public String extractUserName(String token) {
         return "";
@@ -17,5 +29,14 @@ public class JwtServicweIMPL implements JWTservice {
     @Override
     public boolean validateToken(String token, UserDetails user) {
         return false;
+    }
+    private <T> T ectractClaim(String token , Function<Claims,T> getSubject){
+        getClames(token);
+    }
+
+    private Claims getClames(String token) {
+        Jwts.parser().setSigningKey();
+    }
+    private Key getKey(String token){
     }
 }
